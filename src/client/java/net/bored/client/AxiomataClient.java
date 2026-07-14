c6c591f1476df14868a1e7f46f3c76236fdcbb7b
BoredWasRobbed <bored7160@gmail.com>
2026-07-14 10:55:12 -0400

    Initial commit

---
+++ src/client/java/net/bored/client/AxiomataClient.java
@@ -0,0 +1,10 @@
+package net.bored.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AxiomataClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/bored/client/AxiomataDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bored.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AxiomataDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
